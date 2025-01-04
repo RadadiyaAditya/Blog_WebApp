eb44ab630f2358493e2342b7c40cf781bebf25e0
Aditya Radadiya <adityaradadiya294@gmail.com>
2025-01-04 11:06:36 +0530

    User registration feature

---
+++ users/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+#this file is use to customize the login or signup form by inheriting the already existing form
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    #this Meta class defines where to store the data from our form
+    #by form.save() the data will be stored in following models provided in this class
+    #here the user model is changed and the fields only specified here will be shown in the form
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
