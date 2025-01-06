6c6304e65e9d0bb5c846667c10912b82e3c509c7
Aditya Radadiya <adityaradadiya294@gmail.com>
2025-01-06 13:48:40 +0530

    user authentication system

--- django_projects/urls.py
+++ django_projects/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+#views for user login frontend
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('',include('users.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name="users_temp/login.html"), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="users_temp/logout.html"), name='logout'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='user-register')
+    path('register/', views.register, name='user-register'),
+    path('profile/', views.profile, name='profile')
 ]
